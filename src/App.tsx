eb66fb79e347ed58d6b7ff3a87ad9ed6ef630e51
Daylan Berry <daylanberry@Mac.lan>
2025-08-24 19:32:04 -0700

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import "./mobile-emergency-fix.css";
 
@@ -22,6 +23,7 @@ function App() {
 
   return (
     <div className="App">
+      <Analytics />
       <Header
         activeSection={activeSection}
         onSectionChange={handleSectionChange}
